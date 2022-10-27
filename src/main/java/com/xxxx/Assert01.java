f993e387a016cf0b902eb05fa705deba170794cd
zzx <2627250538@qq.com>
2022-10-27 21:06:17 +0800

    s

---
+++ src/main/java/com/xxxx/Assert01.java
@@ -0,0 +1,10 @@
+package com.xxxx;
+
+public class Assert01 {
+    public static void main(String[] args) {
+        System.out.println("Assert01.main");
+        System.out.println("Assert01.main");
+        System.out.println("Assert01.main");
+        System.out.println("Assert01.main");
+    }
+}
